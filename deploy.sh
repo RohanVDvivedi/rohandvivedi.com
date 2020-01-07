424dc53147497d1ef34b3f04bd9049c5d4973f16
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-01-07 15:38:41 +0100

    adde=d comments in the deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,17 @@
-kill -9 $(lsof -t -i:8080)
+# kill the application that is running currently on port 80
+kill -9 $(lsof -t -i:80)
+
+# checkout master branch
 git checkout master
+
+# pull new changes on master
 git pull
+
+# install for newly added go packages 
+# go get
+
+# install for newly added node packages 
 npm install
+
+# run the application again
 sh ./run.sh
