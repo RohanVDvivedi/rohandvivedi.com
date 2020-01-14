979b27265d1b67bcc0f4b426e142d2a15deaa985
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-01-14 14:24:48 +0100

    a more sophisticated deployment script

--- deploy.sh
+++ deploy.sh
@@ -4,14 +4,19 @@ git checkout master
 # pull new changes on master
 git pull
 
-# kill the application that is running currently on port 80
-kill -9 `sudo lsof -t -i tcp:80`
-
 # install for newly added go packages 
 # go get
 
 # install for newly added node packages 
 npm install --no-optional
+
+# kill the application that is running currently on port 80
+APP_PIDS_RUNNING_ON_PORT_80=`sudo lsof -t -i tcp:80`
+if [[ ! -z $APP_PIDS_RUNNING_ON_PORT_80 ]] 
+then
+    echo "sigkill on pids : " $APP_PIDS_RUNNING_ON_PORT_80
+    kill -9 $APP_PIDS_RUNNING_ON_PORT_80
+fi
 
 # run the application again
 sh ./run.sh
