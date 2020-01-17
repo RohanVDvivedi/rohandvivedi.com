aad62da6383e2010623ec00af06fb002c1e58ebc
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-01-17 11:03:36 +0100

    beautified a bit more

--- rjs/content/AlwaysStayBar.js
+++ rjs/content/AlwaysStayBar.js
@@ -2,15 +2,21 @@ import React from "react";
 
 export default class AlwaysStayBar extends React.Component {
     render() {
+        var container_style = {
+            justifyContent: "space-between",
+        };
         return (
-            <div id="container" class="container_style">
-                <a id="home" class="component_style float-left">Home</a>
-                <a id="search_box" class="component_style float-left">
+            <div id="container" class="container_style flex-row-container" style={container_style}>
+                <a id="home" class="component_style">Home</a>
+                <a id="search_box" class="component_style">
                     Search Box
                 </a>
-                <a id="sign_in" class="component_style float-right">Sign in</a>
-                <a id="sign_up" class="component_style float-right">Sign up</a>
-                <div class="float-clear"></div>
+                <div class="flex-row-container" style={{
+                    justifyContent: "flex-end",
+                }}>
+                    <a id="sign_in" class="component_style">Sign in</a>
+                    <a id="sign_up" class="component_style">Sign up</a>
+                </div>
             </div>
         );
     }
--- rjs/content/ScrollAwayBar.js
+++ rjs/content/ScrollAwayBar.js
@@ -2,12 +2,19 @@ import React from "react";
 
 export default class ScrollAwayBar extends React.Component {
     render() {
+        var container_style = {
+            padding: "0 10px",
+            justifyContent: "flex-end",
+        };
+        var component_style = {
+            width: "10%",
+        };
         return (
-            <div id="container" class="container_style flex-row-container">
-                <a id="about" class="component_style">About</a>
-                <a id="contact" class="component_style">Contact</a>
-                <a id="chat" class="component_style">Chat</a>
-                <a id="email" class="component_style">Email</a>
+            <div id="container" class="container_style flex-row-container" style={container_style}>
+                <a id="about" class="component_style" style={component_style}>About</a>
+                <a id="contact" class="component_style" style={component_style}>Contact</a>
+                <a id="chat" class="component_style" style={component_style}>Chat</a>
+                <a id="email" class="component_style" style={component_style}>Email</a>
             </div>
         );
     }
