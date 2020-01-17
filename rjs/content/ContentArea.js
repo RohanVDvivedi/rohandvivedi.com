e3689bed908f5719e7ff630da7839ffaf902e837
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-01-17 10:21:16 +0100

    appropriate css with color scheme added

--- rjs/content/ContentArea.js
+++ rjs/content/ContentArea.js
@@ -4,7 +4,57 @@ export default class ContentArea extends React.Component {
     render() {
         //if(this.props.viewname == "Hello") {
             return (
+                <div>
                 <div id="content_area">Hello</div>
+                <div id="content_area">Hello</div>
+                <div id="content_area">Hello</div>
+                <div id="content_area">Hello</div>
+                <div id="content_area">Hello</div>
+                <div id="content_area">Hello</div>
+                <div id="content_area">Hello</div>
+                <div id="content_area">Hello</div>
+                <div id="content_area">Hello</div>
+                <div id="content_area">Hello</div>
+                <div id="content_area">Hello</div>
+                <div id="content_area">Hello</div>
+                <div id="content_area">Hello</div>
+                <div id="content_area">Hello</div>
+                <div id="content_area">Hello</div>
+                <div id="content_area">Hello</div>
+                <div id="content_area">Hello</div>
+                <div id="content_area">Hello</div>
+                <div id="content_area">Hello</div>
+                <div id="content_area">Hello</div>
+                <div id="content_area">Hello</div>
+                <div id="content_area">Hello</div>
+                <div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div>
+                <div id="content_area">Hello</div>
+                <div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div>
+                <div id="content_area">Hello</div>
+                <div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div>
+                <div id="content_area">Hello</div>
+                <div id="content_area">Hello</div>
+                <div id="content_area">Hello</div>
+                <div id="content_area">Hello</div>
+                <div id="content_area">Hello</div>
+                <div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div>
+                <div id="content_area">Hello</div>
+                <div id="content_area">Hello</div>
+                <div id="content_area">Hello</div>
+                <div id="content_area">Hello</div>
+                <div id="content_area">Hello</div>
+                <div id="content_area">Hello</div>
+                <div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div>
+
+                <div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div>
+                <div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div>
+                <div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div>
+
+
+                <div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div>
+                <div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div>
+                <div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div><div id="content_area">Hello</div>
+                </div>
             );
         //}
     }
