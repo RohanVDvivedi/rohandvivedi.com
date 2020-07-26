add13e927593d6075d02a8edb01bdd749be60b7d
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-07-26 11:27:56 +0530

    the about page looks better than it was before, image is smaller now, centered to the page, page is set to appropriate size

--- rjs/content/contentarea/AboutContent.js
+++ rjs/content/contentarea/AboutContent.js
@@ -10,8 +10,7 @@ class AboutParagraph extends React.Component {
                 fontSize: this.props.size + "px",
                 fontFamily: "lato,sans-serif",
 				fontStyle: "italic",
-                padding: "6px",
-                fontWeight: 500,
+                fontWeight: "500",
                 color: "#323232"
             }}>
                     {this.props.children}
@@ -41,27 +40,27 @@ class ColoredBoldWord extends React.Component {
 export default class AboutContent extends React.Component {
     render() {
         return (
-            <div class="content-root-background content-screen-widthed content-screen-heighted">
-                <div class="behind-nav"></div>
+            <div class="content-root-background content-screen-widthed content-screen-heighted"
+                style={{    display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                        }}>
 
-                <div id={this.props.selected.toLowerCase() + "-content"} class="flex-row-container"
-                    style={{
-                        justifyContent: "center",
-                        alignItems: "center",
-                    }}>
-                    <img src={"/img/me.png"} style={{
-                        height: "200px",
-                        width: "200px"
-                    }}></img>
-                	<div class="flex-col-container"
-                	   style={{justifyContent: "center",}}>
-                    	<AboutParagraph size={26}>Hi, I am Rohan.</AboutParagraph>
-                    	<AboutParagraph size={26}>I am a Software and Hardware Developer.</AboutParagraph>
-                    	<AboutParagraph size={24}>Predominantly a <ColoredBoldWord color="var(--color6)">Backend Developer</ColoredBoldWord>,<br/> who also indulges in building crappy <ColoredBoldWord color="var(--color6)">Frontend</ColoredBoldWord>s like this one.</AboutParagraph>
-                    	<AboutParagraph size={24}>My interests also include <ColoredBoldWord color="var(--color6)"> Embedded Systems, Databases,<br/> Computer Vision, Robotics</ColoredBoldWord> and <ColoredBoldWord color="var(--color6)">FPGAs</ColoredBoldWord>.</AboutParagraph>
-                    	<AboutParagraph size={22}>Find my Curriculum Vitae <a href={window.location.origin.toString() + "/pdf/rohan_cv.pdf"} target="_blank">here</a>.</AboutParagraph>
+                    <div style={{height: "20%", minHeight: "180px"}}>
+                        <div class="flex-row-container" style={{height: "100%"}}>
+
+                            <img src={"/img/me_500h.jpg"} style={{height: "100%"}}/>
+                            
+                        	<div class="flex-col-container"
+                        	   style={{justifyContent: "space-between"}}>
+                            	<AboutParagraph size={18}>Hi, I am Rohan.</AboutParagraph>
+                            	<AboutParagraph size={18}>I am a Software and Hardware Developer.</AboutParagraph>
+                            	<AboutParagraph size={18}>Predominantly a <ColoredBoldWord color="var(--color6)">Backend Developer</ColoredBoldWord>,<br/> who also indulges in building crappy <ColoredBoldWord color="var(--color6)">Frontend</ColoredBoldWord>s like this one.</AboutParagraph>
+                            	<AboutParagraph size={18}>My interests also include <ColoredBoldWord color="var(--color6)"> Embedded Systems, Databases,<br/> Computer Vision, Robotics</ColoredBoldWord> and <ColoredBoldWord color="var(--color6)">FPGAs</ColoredBoldWord>.</AboutParagraph>
+                            	<AboutParagraph size={18}>Find my Curriculum Vitae <a href={window.location.origin.toString() + "/pdf/rohan_cv.pdf"} target="_blank">here</a>.</AboutParagraph>
+                            </div>
+                        </div>
                     </div>
-                </div>
 
             </div>
         );
