c9fc06b955f5aa9a22153b402656b542023ba6b5
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-07-30 18:12:28 +0530

    contact content cetered

--- rjs/content/contentarea/ContactContent.js
+++ rjs/content/contentarea/ContactContent.js
@@ -3,29 +3,31 @@ import React from "react";
 export default class ContactContent extends React.Component {
     render() {
         return (
-        	<div class="content-root-background content-screen-widthed content-screen-heighted">
-	            <div id={this.props.selected.toLowerCase() + "-content"} class="content-component">
-	            	<div>
-		                <div>
-		                    <div>Formal address</div>
-		                </div>
-		                <div>
-		                    <div>Formal phone number</div>
-		                </div>
-		                <div>
-		                    <div>Linked In Icon</div>
-		                    <div>Github</div>
-		                    <div>Download curriculum vitae</div>
-		                </div>
-		                <div>
-		                    <div>Email Icon
-		                        <div>Send Email now Icon, </div>
-		                        <div>also Copies Email To Clipboard</div>
-		                    </div>
-		                    <div>rohandvivedi@gmail.com</div>
-		                </div>
+        	<div class="content-root-background content-screen-widthed content-screen-heighted flex-col-container"
+        		style={{justifyContent: "center",
+                        alignItems: "center",}}>
+		            <div>
+		            	<div>
+			                <div>
+			                    <div>Formal address</div>
+			                </div>
+			                <div>
+			                    <div>Formal phone number</div>
+			                </div>
+			                <div>
+			                    <div>Linked In Icon</div>
+			                    <div>Github</div>
+			                    <div>Download curriculum vitae</div>
+			                </div>
+			                <div>
+			                    <div>Email Icon
+			                        <div>Send Email now Icon, </div>
+			                        <div>also Copies Email To Clipboard</div>
+			                    </div>
+			                    <div>rohandvivedi@gmail.com</div>
+			                </div>
+			            </div>
 		            </div>
-	            </div>
 	        </div>
         );
     }
