54c96647c9540715496d8c181db11d17de156d93
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-07-30 20:19:14 +0530

    content of contacts page finalized

--- rjs/content/contentarea/ContactContent.js
+++ rjs/content/contentarea/ContactContent.js
@@ -13,12 +13,16 @@ export default class ContactContent extends React.Component {
                     	<a href="https://github.com/RohanVDvivedi" target="_blank" style={{display: "block",}}>
 	                        <Icon height="35px" width="35px" iconPath="/icon/github.png"/>
 	                    </a>
-	                    <a href="https://www.youtube.com/channel/UCgn_REjbUH2Dm8CaOXvajJg?view_as=subscriber" target="_blank" style={{display: "block",}}>
-	                        <Icon height="35px" width="35px" iconPath="/icon/youtube.png"/>
-	                    </a>
 	                    <a href="https://www.linkedin.com/in/rohan-dvivedi-ab3014128/" target="_blank" style={{display: "block",}}>
 	                        <Icon height="35px" width="35px" iconPath="/icon/linkedin.png"/>
 	                    </a>
+	                </div>
+	                <div class="flex-row-container">
+	                    <a href="https://www.youtube.com/channel/UCgn_REjbUH2Dm8CaOXvajJg?view_as=subscriber" target="_blank" style={{display: "block",}}>
+	                        <Icon height="35px" width="35px" iconPath="/icon/youtube.png"/>
+	                    </a>
+	                </div>
+	                <div class="flex-row-container">
 	                    <a href="https://www.facebook.com/rohan.dvivedi.961" target="_blank" style={{display: "block",}}>
 	                        <Icon height="35px" width="35px" iconPath="/icon/facebook.png"/>
 	                    </a>
@@ -32,28 +36,16 @@ export default class ContactContent extends React.Component {
 	                    </a>
 			        </div>
 			        <div class="flex-row-container">
+			        	<div>rohandvivedi.gamil.com</div>
 			        	<a onClick={()=>{CopyToClipboard("rohandvivedi@gmail.com")}} style={{display: "block",}}>
-	                        <Icon height="35px" width="35px" iconPath="/icon/gmail.png"/>
+	                        <Icon height="35px" width="35px" iconPath="/icon/mail.png"/>
 	                    </a>
 			        	<a href="https://mail.google.com/mail/u/0/?view=cm&fs=1&tf=1&to=rohandvivedi@gmail.com&su=Hi&body=Dear%20Rohan,%0D%0A%0D%0ALooking%20forward%20to%20hearing%20from%20you,%0D%0AYours%20sincerely." target="_blank" style={{display: "block",}}>
 	                        <Icon height="35px" width="35px" iconPath="/icon/gmail.png"/>
 	                    </a>
 			        </div>
-		            <div>
-		            	<div>
-			                
-			                <div>
-			                    <div>Formal phone number</div>
-			                </div>
-			                
-			                <div>
-			                    <div>Email Icon
-			                        <div>Send Email now Icon, </div>
-			                        <div>also Copies Email To Clipboard</div>
-			                    </div>
-			                    <div>rohandvivedi@gmail.com</div>
-			                </div>
-			            </div>
+		            <div class="flex-row-container">
+		            	<div>+91-6352496059</div>
 		            </div>
 	        </div>
         );
