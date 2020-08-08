c1d5e3628bd5e3989c8e74d77f442e8965f17bdd
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-08-08 23:29:15 +0530

    started organizing my past

--- rjs/content/PastContent.js
+++ rjs/content/PastContent.js
@@ -1,42 +1,5 @@
 import React from "react";
 
-class AboutParagraph extends React.Component {
-    constructor(props) {
-        super(props)
-    }
-    render() {
-        return(
-            <div style={{
-                fontSize: this.props.size + "px",
-                fontFamily: "lato,sans-serif",
-				fontStyle: "italic",
-                fontWeight: "500",
-                color: "#323232"
-            }}>
-                    {this.props.children}
-            </div>
-        );
-    }
-}
-
-class ColoredBoldWord extends React.Component {
-    constructor(props) {
-        super(props)
-    }
-    render() {
-        return(
-            <span style={{
-                fontSize: "inherit",
-                fontFamily: "inherit",
-                color: this.props.color,
-                fontWeight: "bold",
-            }}>
-                    {this.props.children}
-            </span>
-        );
-    }
-}
-
 export default class PastContent extends React.Component {
     render() {
         return (
@@ -44,22 +7,55 @@ export default class PastContent extends React.Component {
                 style={{justifyContent: "center",
                         alignItems: "center",}}>
 
-                    <div>
-                        <div>
+                    <div class="set_sub_content_background_color generic-content-box-border"
+                    	style={{ width: "60%", padding: "1.5%",}}>
+
+                        <div style={{
+                        	textAlign: "center",
+							fontFamily: "lato, sans-serif",
+							fontSize: "25px",
+							color: "rgb(50,50,50)",
+							fontWeight: "600",
+							fontStyle: "italic",
+                        }}>
                             Past
                         </div>
                         <div>
-                            Work Experience :
-                            <a href={"https://www.oyorooms.com/"}>OYO</a>
-                            Job Description: Fullstack developer contributing in php, java and Progress to Belvilla (now OYO Vacation Homes).
-                            Team : OYO Vacation Homes Team, Amsterdam, Netherlands      Jul’19-Feb’20
-                            Job Description: Backend developer contributing in java and ruby tachstack to Finance Technology teams in generating reconciliation summary (accounting for commission, payments and taxations).
-                            Team : Finance Tech. Team, Gurgaon, India                   Jan’19-Jun’19
-                            Team : Supply Tech. Team, Gurgaon, India                    Aug’18-Dec’18
-                            Research Experience :
-                            <a href={"https://ieeexplore.ieee.org/document/9008052"}>Flexible Processor Architecture Design</a>
-                            Description: Designing a processor that can execute any instruction set. Allowing the programmers to come up with their own custom instruction sets targetting the needs of their application.
+                            <div>Work Experience</div>
+                            <a href={"https://www.oyorooms.com/"} target="_blank">OYO</a>
+
+                            <div>
+                            	<div>Job Description: Fullstack Developer contributing in php, java and Progress to Belvilla (now OYO Vacation Homes).</div>
+                            	<div>OYO Vacation Homes Team, Amsterdam, Netherlands</div>
+                            	<div>Jul’19-Feb’20</div>
+                            </div>
+
+                            <div>
+                            	<div>Job Description: Backend Developer contributing in java and ruby tachstack to Finance Technology teams in generating reconciliation summary (accounting for commission, payments and taxations).</div>
+                            	<div>Team : Finance Tech. Team, Gurgaon, India</div>
+                            	<div>Jan’19-Jun’19</div>
+                            </div>
+                            <div>
+                            	<div>Job Description: Backend Developer contributing in java and ruby tachstack to Supply Technology teams.</div>
+                            	<div>Team : Supply Tech. Team, Gurgaon, India</div>
+                            	<div>Aug’18-Dec’18</div>
+                            </div>
                         </div>
+
+                        <div>
+                            <div>Research Experience</div>
+                            <a href="https://ieeexplore.ieee.org/document/9008052" target="_blank">Flexible Processor Architecture Design</a>
+                            <div>Abstract: Designing a processor that can execute any instruction set. Allowing the programmers to come up with their own custom instruction sets targetting the needs of their application.</div>
+                            <div>Jul’17-Dec’17</div>
+                        </div>
+
+                        <div>
+                            <div>Undergraduate Education</div>
+                            <a href="https://www.bits-pilani.ac.in/hyderabad/" target="_blank">BITS Pilani</a>
+                            <div>B.E. (Hons.) in Mechanical Engineering</div>
+                            <div>Jul’14-Jul’18</div>
+                        </div>
+
                     </div>
 
             </div>
