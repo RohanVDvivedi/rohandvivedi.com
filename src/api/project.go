5186fe44e6b4f3ed91467c6383587e659cd2bfbd
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-08-12 23:09:23 +0530

    nulls allowed in system

--- src/api/project.go
+++ src/api/project.go
@@ -6,6 +6,12 @@ import (
 	"rohandvivedi.com/src/data"
 )
 
+type Project struct {
+	data.Project
+	Hyperlinks []data.ProjectHyperlink
+	Categories []data.ProjectCategory
+}
+
 func FindProject(w http.ResponseWriter, r *http.Request) {
 	project_name := r.URL.Query().Get("name");
 	//project_type := r.URL.Query().Get("type");
@@ -16,4 +22,16 @@ func FindProject(w http.ResponseWriter, r *http.Request) {
 		json, _ := json.Marshal(*project);
 		w.Write(json);
 	}
+}
+
+func FindProjectByName(projectName string) *Project {
+	return nil;
+}
+
+func FindProjectsForSearchStringInCategories(queryString string, categories []string) []Project {
+	return []Project{};
+}
+
+func FindProjectsByCategories(categories []string) []Project {
+	return []Project{};
 }
