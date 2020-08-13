79d27fbb230316bc338e35473348c245b6bbb3d2
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-08-13 10:19:35 +0530

    provided a simple all round dto and api for the projects module

--- src/api/project.go
+++ src/api/project.go
@@ -13,19 +13,39 @@ type Project struct {
 }
 
 func FindProject(w http.ResponseWriter, r *http.Request) {
-	project_name := r.URL.Query().Get("name");
-	//project_type := r.URL.Query().Get("type");
 
-	project := data.GetProjectByName(project_name);
+	var project_p *Project = nil;
+	//var projects []Project;
 
-	if(project != nil) {
-		json, _ := json.Marshal(*project);
+	name, exists_name := r.URL.Query()["name"];
+	if (exists_name) {
+		requested_hyperlinks, exists_get_hyperlinks := r.URL.Query()["get_hyperlinks"];
+		var withHyperlinks bool = exists_get_hyperlinks && (requested_hyperlinks[0] == "true")
+		project_p = FindProjectByName(name[0], withHyperlinks)
+	}
+
+	if(project_p != nil) {
+		json, _ := json.Marshal(*project_p);
 		w.Write(json);
+	} else {
+		w.Write([]byte("{}"));
 	}
 }
 
-func FindProjectByName(projectName string) *Project {
-	return nil;
+func FindProjectByName(name string, withHyperlinks bool) *Project {
+	proj_db := data.GetProjectByName(name)
+	if(proj_db != nil) {
+		p := &Project{};
+		p.Project = *proj_db
+
+		// if hyperlinks are requested, add hyperlinks to the project dto
+		if withHyperlinks {
+			p.Hyperlinks = proj_db.GetHyperlinks();
+		}
+
+		return p;
+	}
+	return nil
 }
 
 func FindProjectsForSearchStringInCategories(queryString string, categories []string) []Project {
@@ -35,3 +55,5 @@ func FindProjectsForSearchStringInCategories(queryString string, categories []st
 func FindProjectsByCategories(categories []string) []Project {
 	return []Project{};
 }
+
+//
