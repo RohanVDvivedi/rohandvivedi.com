ca75ee9d01421c0db7bd1ac2e1387aaeac763cd1
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-08-16 21:39:41 +0530

    kill previous deplyment or any other appplication using any or port 80 or 443

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,8 @@ go get github.com/mattn/go-sqlite3
 # install for newly added node packages 
 npm install --no-optional
 
+# we own the Fucking server, kill any other application that is competing for 80 or 443 port
+
 # kill the application that is running currently on port 80
 APP_PIDS_RUNNING_ON_PORT_80=`sudo lsof -t -i tcp:80`
 if [ $APP_PIDS_RUNNING_ON_PORT_80 ] 
@@ -21,6 +23,16 @@ then
     kill -9 $APP_PIDS_RUNNING_ON_PORT_80
 else
     echo "no process to be killed to acquire port tcp:80"
+fi
+
+# kill the application that is running currently on port 443
+APP_PIDS_RUNNING_ON_PORT_443=`sudo lsof -t -i tcp:443`
+if [ $APP_PIDS_RUNNING_ON_PORT_443 ] 
+then
+    echo "sigkill on pids : " $APP_PIDS_RUNNING_ON_PORT_443
+    kill -9 $APP_PIDS_RUNNING_ON_PORT_443
+else
+    echo "no process to be killed to acquire port tcp:443"
 fi
 
 # run the application again
