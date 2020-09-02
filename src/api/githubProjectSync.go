cac3473864e8c32288aef347dd4a7eff74aba3d9
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-09-02 11:33:13 +0530

    added pseudo code to setup project and hyperlink from github data

--- src/api/githubProjectSync.go
+++ src/api/githubProjectSync.go
@@ -12,26 +12,31 @@ var SyncProjectFromGithubRepository = http.HandlerFunc(githubRepositorySyncUp)
 
 func githubRepositorySyncUp(w http.ResponseWriter, r *http.Request) {
 	projectName, exists_name := r.URL.Query()["name"];
-	if exists_name {
-		projGithub, err := githubsync.GetGithubProject("RohanVDvivedi", projectName[0]);
-		if(err != nil) {
-			w.WriteHeader(http.StatusBadRequest)
-			w.Write([]byte("No such Github repository exists for the owner on Github"))
+	if !exists_name {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("You must provide name of the project"))
+		return
+	}
+
+	// find appropriate project on gihub of the owner
+	projGithub, err := githubsync.GetGithubProject("RohanVDvivedi", projectName[0]);
+	if(err != nil) {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("No such Github repository exists for the owner on Github"))
+		return
+	}
+
+	// update or insert project details of owner, from the github api call
+	projdb := data.GetProjectByName(projectName[0])
+	if(projdb == nil) {
+		// insert project and project_hyperlink of the github repository
+	} else {
+		// update project
+		proj_hyperlinkdb := projdb.GetProjectGithubRepositoryLink())
+		if(proj_hyperlinkdb == nil) {
+			// insert github link
 		} else {
-			projdb := data.GetProjectByName(projectName[0])
-			if(projdb == nil) {
-				// insert
-				fmt.Println("inserting project");
-				fmt.Println(projGithub);
-			} else {
-				// update
-				fmt.Println("updating project");
-				fmt.Println(projdb);
-				fmt.Println("using");
-				fmt.Println(projGithub);
-				fmt.Println("repo link = ")
-				fmt.Println(projdb.GetProjectGithubRepositoryLink())
-			}
+			// update github link
 		}
 	}
 }
