a3bc7bf20619c266b50f9b0029356ca87702d4f1
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-09-09 18:09:28 +0530

    separated project search bar from project content js file

--- rjs/content/ProjectsContent.js
+++ rjs/content/ProjectsContent.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import ProjectSearchBar from "./ProjectSearchBar";
+
 import ApiComponent from "../utility/ApiComponent";
 import Icon from "../utility/Icon";
 
@@ -54,116 +56,6 @@ class ProjectListerComponent extends React.Component {
             </div>
         );
     }
-}
-
-class ProjectSearchBar extends ApiComponent {
-	constructor(props) {
-		super(props)
-		this.state =  Object.assign({} ,this.state, {
-			searchTextBox: "",
-			showDropdownContent: false,
-			selectedProjectCategories: [],
-		})
-	}
-	apiPath() {
-        return "/api/all_categories";
-    }
-    bodyDataBeforeApiFirstResponds() {
-    	return [];
-    }
-	categoryClicked(category) {
-		var selects = [];
-		if(category == "") {
-			selects = [];
-		} else {
-			selects = Array.from(this.state.selectedProjectCategories);
-			if(selects.includes(category)) {
-				selects = selects.filter((cate) => {return cate != category});
-			} else {
-				selects.push(category);
-				if(selects.length == this.categories.length){
-					selects = []
-				}
-			}
-		}
-		this.updateState({
-				searchTextBox: this.state.searchTextBox,
-				showDropdownContent: selects.length > 0,
-				selectedProjectCategories: selects,
-			});
-	}
-	onCategoryDrawerClicked() {
-		this.updateState({
-				searchTextBox: this.state.searchTextBox,
-				showDropdownContent: !this.state.showDropdownContent,
-				selectedProjectCategories: this.state.selectedProjectCategories,
-			});
-	}
-	onSearchBoxTyping(e) {
-		this.updateState({
-				searchTextBox: e.target.value,
-				showDropdownContent: this.state.showDropdownContent,
-				selectedProjectCategories: this.state.selectedProjectCategories,
-			});
-	}
-	generateSearchQueryString() {
-		var searchQuery = new Array();
-
-		var searchText = this.state.searchTextBox;
-		if(searchText != null && searchText != ""){
-			searchQuery[0] = "query=" + searchText;
-		}
-
-		var categories = ((this.state.selectedProjectCategories.length == 0) ? this.categories : this.state.selectedProjectCategories);
-		if(categories != null && categories != ""){
-			searchQuery[1] = "categories=" + categories.join(",");
-		}
-
-		// if search is pressed close the drop down
-		this.updateState({
-				searchTextBox: this.state.searchTextBox,
-				showDropdownContent: false,
-				selectedProjectCategories: this.state.selectedProjectCategories,
-			});
-
-		return searchQuery.filter((q) => {return q != null && q != ""}).join("&");
-	}
-	searchButtonClicked() {
-		var queryString = this.generateSearchQueryString();
-		this.props.searchQueryStringBuiltCallback(queryString);
-	}
-	enterKeyClicked(event) {
-		if (event.keyCode === 13) {event.preventDefault();
-			this.searchButtonClicked();}
-	}
-	render() {
-		// convert list of objects to list of strings
-		this.categories = this.state.api_response_body.map(function(category){return category.Category});
-		return (<div style={{display:"flex",justifyContent:"center"}}>
-					<div class="search-container flex-row-container set_sub_content_background_color">
-	                	<input class="search-text-selector" type="text" 
-	                		placeholder="Search projects" value={this.state.searchTextBox}
-	                		onChange={this.onSearchBoxTyping.bind(this)} 
-	                		onKeyUp={this.enterKeyClicked.bind(this)}/>
-						<div class={"search-categories-selector dropdown-container generic-content-box-hovering-emboss-border " + (this.state.showDropdownContent ? "show-dropdown-content" : "") }>
-							<div class="flex-row-container" style={{justifyContent: "space-evenly", alignItems: "center"}} onClick={this.onCategoryDrawerClicked.bind(this)}>
-								<div>Categories</div>
-								<Icon path="#" iconPath="/icon/up-chevron.png" height="15px" width="15px" padding="2px" />
-							</div>
-							<div class="dropdown-content set_sub_content_background_color">
-								<div id="select-all-categories" class={"search-category " + ((this.state.selectedProjectCategories.length==0) ? "active" : "")}
-								 onClick={this.categoryClicked.bind(this, "")}>All</div>
-								{this.categories.map((projectCategory) => {
-									return (<div class={"search-category " + ((this.state.selectedProjectCategories.includes(projectCategory)) ? "active" : "")}
-										onClick={this.categoryClicked.bind(this, projectCategory)}>{projectCategory}</div>);
-								})}
-							</div>
-						</div>
-	                	<div class="search-button generic-content-box-hovering-emboss-border" 
-	                		onClick={this.searchButtonClicked.bind(this)}>Search</div>
-	                </div>
-                </div>);
-	}
 }
 
 export default class ProjectsContent extends ApiComponent {
