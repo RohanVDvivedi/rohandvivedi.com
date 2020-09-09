af1e1ff884be220d96d633af1e4f0a3db25fbaa9
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-09-09 15:34:44 +0530

    restructured the way hoe project div is shown

--- rjs/content/ProjectsContent.js
+++ rjs/content/ProjectsContent.js
@@ -8,21 +8,39 @@ class ProjectListerComponent extends React.Component {
     	var hyperlinks = (this.props.project.Hyperlinks == null) ? [] : this.props.project.Hyperlinks
     	var thumbImage = hyperlinks.find((link) => {return link.Name.toLowerCase() == "thumbnail" && link.LinkType == "IMAGE"})
     	var GithubRepository = hyperlinks.find((link) => {return link.Name == this.props.project.Name && link.LinkType == "GITHUB"})
+
+    	var GithubRepositories = hyperlinks.filter((link) => {return link.LinkType == "GITHUB"});
+    	var YoutubeVideos = hyperlinks.filter((link) => {return link.LinkType == "YOUTUBE"});
         return (
-            <div class="project-lister-element flex-col-container set_sub_content_background_color generic-content-box-border">
+            <div class="project-lister-element flex-col-container set_sub_content_background_color generic-content-box-border"
+            		style={{
+            			justifyContent: "space-between"
+            		}}>
                 <h1 class="project-lister-element-name">{this.props.project.Name}</h1>
 
                 {(thumbImage != null) ? (<img class="project-lister-element-image" src={thumbImage.Href}/>) : ""}
 
 	            <h3 class="project-lister-element-description">{this.props.project.Descr}</h3>
 
-                <div class="flex-row-container" style={{justifyContent: "space-around",
-                										alignItems: "center",}}>
-                	{hyperlinks.filter((link) => {return link.LinkType != "IMAGE"}).map((link) => {
-                		return (<Icon path={link.Href} iconPath={"/icon/" + link.LinkType.toLowerCase() + ".png"} 
-                				infoBoxText={link.LinkType == "GITHUB" ? link.Name : link.Descr} height="35px" width="35px" padding="5px" />)
-                	})}
-                </div>
+				{GithubRepositories != null && GithubRepositories.length > 0 ?
+                	(<div class="flex-row-container" style={{justifyContent: "space-around",
+                											alignItems: "center",}}>
+                		{GithubRepositories.map((link) => {
+                			return (<Icon path={link.Href} iconPath={"/icon/github.png"} 
+                					infoBoxText={link.Name} height="35px" width="35px" padding="5px" />)
+                		})}
+                	</div>) : ""
+            	}
+
+				{YoutubeVideos != null && YoutubeVideos.length > 0 ?
+					(<div class="flex-row-container" style={{justifyContent: "space-around",
+                											alignItems: "center",}}>
+                		{YoutubeVideos.map((link) => {
+                			return (<Icon path={link.Href} iconPath={"/icon/youtube.png"} 
+                					infoBoxText={link.Descr} height="35px" width="35px" padding="5px" />)
+                		})}
+                	</div>) : ""
+				}
             </div>
         );
     }
