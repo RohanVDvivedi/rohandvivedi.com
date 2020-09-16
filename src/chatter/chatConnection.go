cbe2b0f2b019120d4a664f2057eb41c9869eba79
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-09-16 15:44:50 +0530

    only blocking call support initially for simplicity

--- src/chatter/chatConnection.go
+++ src/chatter/chatConnection.go
@@ -9,37 +9,30 @@ import (
 
 type ChatConnection struct {
 	Id
-	MessagesToBeSent* ChatMessageQueue
+
 	Connection *websocket.Conn
-	ConnectionCloseWait sync.WaitGroup
 	
 	// this is the user that this connection belongs to
 	User *ChatUser
 }
 
-func NewChatConnection() *ChatConnection {
+func NewChatConnection(Connection *websocket.Conn, User *ChatUser) *ChatConnection {
 	return &ChatConnection {
-		Id:GetChatNewConnectionId(),
-		MessagesToBeSent: NewChatMessageQueue(),
+		Id: GetNewChatConnectionId(),
+		Connection: Connection,
+		User: User
 	}
 }
 
-func (cconn *ChatConnection) Start(Connection *websocket.Conn) {
-	cconn.IsActive = true
-	cconn.Connection = Connection
-	cconn.ConnectionCloseWait.Add(1)
-	go cconn.LoopOverChannelToPassMessages()
-}
-
-func (cconn *ChatConnection) SendMessage(msg ChatMessage) {
-	cconn.MessagesToBeSent.Push(msg)
+func (cconn *ChatConnection) SendMessage(msg ChatMessage) error {
+	if(msg.To == cconn.GetId() || msg.To == cconn.User.GetId()) {
+		return ChatMessageCodec.Send(cconn.Connection, msg)
+	}
+	return errors.New("ERROR in routing")
 }
 
 func (cconn *ChatConnection) ReceiveMessage() (ChatMessage, error) {
 	msg := ChatMessage{}
-	if(!cconn.IsActive) {
-		return msg, errors.New("Error connection not active")
-	}
 	err := ChatMessageCodec.Receive(cconn.Connection, &msg)
 	if(err != nil) {	// this could mean, connection closed or malformed chatMessage packet
 		return msg, err
@@ -47,35 +40,6 @@ func (cconn *ChatConnection) ReceiveMessage() (ChatMessage, error) {
 	return msg, nil
 }
 
-func (cconn *ChatConnection) LoopOverToPassMessages() {
-	for (true) {
-		msg := cconn.MessagesToBeSent.Top()
-		if(msg.To == ccon.GetId()) {
-			err := ChatMessageCodec.Send(cconn.Connection, msg)
-			if(err != nil) { // this could mean, connection closed or lost
-				break
-			}
-		}
-		cconn.MessagesToBeSent.Pop()
-	}
-	cconn.ConnectionCloseWait.Done()
-	cconn.Connection.Close()
-}
-
-func (cconn *ChatConnection) WaitForShutdown() {
-	cconn.ConnectionCloseWait.Wait()
-}
-
-func (cconn *ChatConnection) Stop() {
-	cconn.Connection.Close()
-	cconn.WaitForShutdown()
-	cconn.Connection = nil
-	cconn.IsActive = false
-}
-
 func (cconn *ChatConnection) Destroy() {
-	if(cconn.IsActive) {
-		cconn.Stop()
-	}
-	cconn.MessagesToBeSent = nil
+	cconn.Connection.Close()
 }
