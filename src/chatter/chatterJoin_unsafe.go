4236e07c27effe710c811ac48876783022a6def8
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-09-18 10:41:55 +0530

    to simplify the implementation inside the server message handlers, special joined methods are implemented

---
+++ src/chatter/chatterJoin_unsafe.go
@@ -0,0 +1,27 @@
+package chatter
+
+// all the methods below are chatMAnager unsafe and must be called only while holding the lock
+
+func isUserJoinedToGroup(cu *ChatUser, cg *ChatGroup) {
+
+}
+
+func JoinUserToGroup(cu *ChatUser, cg *ChatGroup) {
+
+}
+
+func BreakUserFromGroup(cu *ChatUser, cg *ChatGroup) {
+	
+}
+
+func isConnectionJoinedToUser(cu *ChatUser, cg *ChatGroup) {
+	
+}
+
+func JoinConnectionToUser(cc *ChatConnection, cu *ChatUser) {
+
+}
+
+func BreakConnectionFromUser(cc *ChatConnection, cu *ChatUser) {
+	
+}
