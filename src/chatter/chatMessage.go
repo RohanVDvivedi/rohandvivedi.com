ccfc5ccc2e64bc280ba25fc5438513ef4e239d31
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-09-18 15:06:28 +0530

    added necesssary message fields to the ChatMessage

--- src/chatter/chatMessage.go
+++ src/chatter/chatMessage.go
@@ -13,7 +13,18 @@ type ChatMessage struct {
 	From string 		// id of the sender (can be a chat connection or a chat user)
 	To string 			// id of the receiver (can be a chat connection or a chat user or a chat group)
 	SentAt time.Time 	// when was the message sent (time stamp)
+
+	MessageId string 	// string message id
 	Message string 		// string message content
+	ContextId string 	// string message context id
+
+	/* Things are getting a little shifty here */
+	// MessageId is an attribute to be used by chat users to uniquely identify a message
+	// ContextId is an attribute of a message to be used by receiver to let the sender know what is it the reply or response to
+	// let us say if you are using acknowledgement for message being received or message being read
+	// for those acknowledgement messages the Context id must be equal to the message id of the corresponding message
+	// a reply to a message must carry a ContextId equal to the MessageId of the message that is being replied to
+	// once a server request is processed (log in or log out), the ContextId of the server response must equal to the MessageId of the server request
 }
 
 func EmptyMessage() ChatMessage {
