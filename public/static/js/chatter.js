f085785bd9f37e7521851cbe93d030100bb1dd78
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-09-28 12:20:01 +0530

    corrected indentation

--- public/static/js/chatter.js
+++ public/static/js/chatter.js
@@ -138,13 +138,13 @@ window.Chatter = {
 }
 
 function GetRandomString(length) {
-   var result           = '';
-   var characters       = "+-/<[abcdefghijklmnopqrstuvwxyz](ABCDEFGHIJKLMNOPQRSTUVWXYZ){0123456789}>-_^#";
-   var charactersLength = characters.length;
-   for ( var i = 0; i < length; i++) {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
-   }
-   return result;
+	var result           = '';
+	var characters       = "+-/<[abcdefghijklmnopqrstuvwxyz](ABCDEFGHIJKLMNOPQRSTUVWXYZ){0123456789}>-_^#";
+	var charactersLength = characters.length;
+	for ( var i = 0; i < length; i++) {
+		result += characters.charAt(Math.floor(Math.random() * charactersLength));
+	}
+	return result;
 }
 
 function executeOnlyAFunctionIfNotNull(funcN, ...args) {
