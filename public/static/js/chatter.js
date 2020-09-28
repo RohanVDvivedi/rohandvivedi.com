748b7a169bf10e776354307a0a32f3b667edc377
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-09-28 14:53:49 +0530

    chatter js starting to be fixed

--- public/static/js/chatter.js
+++ public/static/js/chatter.js
@@ -1,16 +1,12 @@
 
-const STATES = {
+window.Chatter = {
+	STATES: {
 		CONNECTED: "CONNECTED",	// messages sent using connection id in the from field
 		LOGGED_IN: "LOGGED_IN",	// messages sent using user id in the from field
 		DISCONNECTED: "DISCONNECTED",	// messages can not be sent
-	}
+	},
 
-function getConnectionUrl() {
-	return (window.location.protocol.includes("https") ? "wss://" : "ws://") + window.location.host + "/soc/chatter"
-}
-
-window.Chatter = {
-	CurrentState: STATES.DISCONNECTED,
+	CurrentState: "DISCONNECTED",
 
 	Connection: null,
 	onOpen: "Socket Connection is now open",
@@ -28,9 +24,12 @@ window.Chatter = {
 	onUserNotification: function(user){console.log("User :", user)},
 	onSearchResultsReady: function(results){console.log("SearchResults :", results)},
 
-	ReqConnection: function(name = null, publicKey = null) {
-		var thiz = Chatter
-		if(thiz.CurrentState != STATES.DISCONNECTED) {
+	getConnectionUrl() {
+		return (window.location.protocol.includes("https") ? "wss://" : "ws://") + window.location.host + "/soc/chatter"
+	},
+
+	ReqConnection(name = null, publicKey = null) {
+		if(this.CurrentState != this.STATES.DISCONNECTED) {
 			return false
 		}
 
@@ -38,102 +37,177 @@ window.Chatter = {
 		if(name != null) {queryParams.push("name=" + name)}
 		if(publicKey != null) {queryParams.push("publicKey=" + publicKey)}
 
-		var URL = [getConnectionUrl(), queryParams.join("&")].join("?")
+		var URL = [this.getConnectionUrl(), queryParams.join("&")].join("?")
 
-		thiz.Connection = new WebSocket(URL);
+		this.Connection = new WebSocket(URL);
 
-		thiz.Connection.onopen = function() {
-			thiz.CurrentState = STATES.CONNECTED
-			executeOnlyAFunctionIfNotNull(thiz.onOpen)
-		}
+		this.Connection.onopen = function() {
+			this.CurrentState = this.STATES.CONNECTED
+			executeOnlyAFunctionIfNotNull(this.onOpen)
+		}.bind(this)
 
-		thiz.Connection.onmessage = function(msgEvent) {
-			ChatterConnectionHandler(thiz, msgEvent)
-		}
+		this.Connection.onmessage = function(msgEvent) {
+			this.chatterConnectionHandler(msgEvent)
+		}.bind(this)
 
-		thiz.Connection.onerror = function(error) {
+		this.Connection.onerror = function(error) {
 			console.log("Error: " + error.message)
-		}
+		}.bind(this)
 
-		thiz.Connection.onclose = function() {
-			thiz.CurrentState = STATES.DISCONNECTED
-			executeOnlyAFunctionIfNotNull(thiz.onClose)
-		}
+		this.Connection.onclose = function() {
+			this.CurrentState = this.STATES.DISCONNECTED
+			executeOnlyAFunctionIfNotNull(this.onClose)
+		}.bind(this)
 
 		return true
 	},
 
-	ReqCreateUser: function(name, publicKey) {
-		var thiz = Chatter
-		if(thiz.CurrentState != STATES.CONNECTED) {
-			return null
+	sendMessageRaw(From,To,Message = null,Messages = null,ContextId = null) {
+		var msg = {
+			From: From,To: To,SentAt: new Date(),
+			Message: Message,Messages: Messages,
+			MessageId: getRandomString(64),ContextId: ContextId
 		}
-		return sendMessageInternal(thiz.ConnectionId,"server-create-and-login-as-chat-user","",[name,publicKey])
-	},
-	ReqLogin: function(name, publicKey) {
-		var thiz = Chatter
-		if(thiz.CurrentState != STATES.CONNECTED) {
-			return null
-		}
-		return sendMessageInternal(thiz.ConnectionId,"server-login-as-chat-user","",[name,publicKey])
-	},
-	ReqLogout: function() {
-		var thiz = Chatter
-		if(thiz.CurrentState != STATES.LOGGED_IN) {
-			return null
-		}
-		return sendMessageInternal(thiz.ConnectionId,"server-logout-from-chat-user")
+		this.Connection.send(JSON.stringify(msg))
+		return msg
 	},
 
-	// a true means a request to send a mesage was successfull
-	// thiz does not ensure delivery
-	SendMessage: function(to, textMsg) {
-		var thiz = Chatter
-		if(thiz.CurrentState != STATES.LOGGED_IN) {
+	ReqCreateUser(name, publicKey) {
+		if(this.CurrentState != this.STATES.CONNECTED) {
+			return null
+		}
+		return this.sendMessageRaw(this.ConnectionId,"server-create-and-login-as-chat-user","",[name,publicKey])
+	},
+	ReqLogin(name, publicKey) {
+		if(this.CurrentState != this.STATES.CONNECTED) {
+			return null
+		}
+		return this.sendMessageRaw(this.ConnectionId,"server-login-as-chat-user","",[name,publicKey])
+	},
+	ReqLogout() {
+		if(this.CurrentState != this.STATES.LOGGED_IN) {
+			return null
+		}
+		return this.sendMessageRaw(this.ConnectionId,"server-logout-from-chat-user")
+	},
+
+	SendMessage(to, textMsg) {
+		if(this.CurrentState != this.STATES.LOGGED_IN) {
 			return null
 		}
 		if(!(typeof(textMsg) === 'string' || textMsg instanceof String)) {
 			return null
 		}
-		return sendMessageInternal(thiz.User.Id,to,textMsg)
+		return this.sendMessageRaw(this.User.Id,to,textMsg)
 	},
 
-	RequestGenericQuery: function(serverReceiverName, queryParam = "") {
-		var thiz = Chatter
-		if(thiz.CurrentState != STATES.LOGGED_IN) {
+	RequestGenericQuery(serverReceiverName, queryParam = "") {
+		if(this.CurrentState != this.STATES.LOGGED_IN) {
 			return null
 		}
-		return sendMessageInternal(thiz.User.Id,serverReceiverName, queryParam)
+		return this.sendMessageRaw(this.User.Id,serverReceiverName, queryParam)
 	},
 
-	ReqGetAllUsers: function() {
-		var thiz = Chatter
-		return thiz.RequestGenericQuery("server-get-all-users")
+	ReqGetAllUsers() {
+		return this.RequestGenericQuery("server-get-all-users")
 	},
 
-	ReqGetAllGroups: function() {
-		var thiz = Chatter
-		return thiz.RequestGenericQuery("server-get-all-groups")
+	ReqGetAllGroups() {
+		return this.RequestGenericQuery("server-get-all-groups")
 	},
 
-	ReqGetAllOnlineUsers: function() {
-		var thiz = Chatter
-		return thiz.RequestGenericQuery("server-get-all-online-users")
+	ReqGetAllOnlineUsers() {
+		return this.RequestGenericQuery("server-get-all-online-users")
 	},
 
-	ReqGetAllMyGroups: function() {
-		var thiz = Chatter
-		return thiz.RequestGenericQuery("server-get-all-my-groups")
+	ReqGetAllMyGroups() {
+		return this.RequestGenericQuery("server-get-all-my-groups")
 	},
 
-	ReqGetAllMyActiveConnections: function() {
-		var thiz = Chatter
-		return thiz.RequestGenericQuery("server-get-all-my-active-connections")
+	ReqGetAllMyActiveConnections() {
+		return this.RequestGenericQuery("server-get-all-my-active-connections")
 	},
 
-	ReqSearch: function(query) {
-		var thiz = Chatter
-		return thiz.RequestGenericQuery("server-search-chatter-box",query)
+	ReqSearch(query) {
+		return this.RequestGenericQuery("server-search-chatter-box",query)
+	},
+
+	/* restricted access */
+	/* Code below is meant to allow the chatter client to follow the standard state transitions and protocol for chattering */
+	/* Access to below source is restricted to only those people who are familiar with chatter protocol */
+
+	chatterConnectionHandler(msgEvent) {
+
+		var msg = JSON.parse(msgEvent.data)
+		msg.SentAt = Date.parse(msg.SentAt)
+
+		if(isServerEvent(msg)) {
+			console.log("Server event", msg)
+			switch(msg.From){
+				case "server-chatters-creator" : {
+					if(isChatConnectionId(msg.To)) {
+						this.ConnectionId = msg.To
+						this.CurrentState = this.STATES.CONNECTED
+						executeOnlyAFunctionIfNotNull(this.onConnected)
+					} else if (isChatUserId(msg.To)) {
+						// user created
+					} else if (isChatGroupId(msg.To)) {
+						// group created
+					}
+					break;
+				}
+				case "server-create-and-login-as-chat-user" :
+				case "server-login-as-chat-user" : {
+					if(!isErrorEvent(msg)) {
+						this.User = GetUserFromString(msg.Message)
+						this.CurrentState = this.STATES.LOGGED_IN
+						executeOnlyAFunctionIfNotNull(this.onLogin)
+					}
+					break;
+				}
+				case "server-logout-from-chat-user" : {
+					if(!isErrorEvent(msg)) {
+						this.User = null
+						this.CurrentState = this.STATES.CONNECTED
+						executeOnlyAFunctionIfNotNull(this.onLogout)
+					}
+					break;
+				}
+				case "server-get-all-users" : {
+					if(!isErrorEvent(msg)) {
+						var results = msg.Messages.map(function(userStr){
+							return GetUserFromString(userStr)
+						}).filter(function(user){
+							return user != null && user.Id != this.User.Id
+						}).bind(this)
+						executeOnlyAFunctionIfNotNull(this.onChangeUsersList, results)
+					}
+					break;
+				}
+				case "server-event-update" : {
+					if(!isErrorEvent(msg)) {
+						var user = GetUserFromString(msg.Message)
+						if(user != null && user.Id != this.User.Id) {
+							executeOnlyAFunctionIfNotNull(this.onUserNotification, user)
+						}
+					}
+					break;
+				}
+				case "server-search-chatter-box" : {
+					if(!isErrorEvent(msg)) {
+						var results = msg.Messages.map(function(userStr){
+							return GetUserFromString(userStr)
+						}).filter(function(user){
+							return user != null
+						})
+						executeOnlyAFunctionIfNotNull(this.onSearchResultsReady, results)
+					}
+					break;
+				}
+			}
+		} else {
+			this.onChatMessage(msg)
+		}
 	},
 }
 
@@ -155,94 +229,7 @@ function executeOnlyAFunctionIfNotNull(funcN, ...args) {
 	}
 }
 
-function sendMessageInternal(From,To,Message = null,Messages = null,ContextId = null) {
-	var msg = {
-		From: From,To: To,SentAt: new Date(),
-		Message: Message,Messages: Messages,
-		MessageId: getRandomString(64),ContextId: ContextId
-	}
-	Chatter.Connection.send(JSON.stringify(msg))
-	return msg
-}
 
-/* restricted access */
-/* Code below is meant to allow the chatter client to follow the standard state transitions and protocol for chattering */
-/* Access to below source is restricted to only those people who are familiar with chatter protocol */
-
-function ChatterConnectionHandler(chatter, msgEvent) {
-
-	var msg = JSON.parse(msgEvent.data)
-	msg.SentAt = Date.parse(msg.SentAt)
-
-	if(isServerEvent(msg)) {
-		console.log("Server event", msg)
-		switch(msg.From){
-			case "server-chatters-creator" : {
-				if(isChatConnectionId(msg.To)) {
-					chatter.ConnectionId = msg.To
-					chatter.CurrentState = STATES.CONNECTED
-					executeOnlyAFunctionIfNotNull(chatter.onConnected)
-				} else if (isChatUserId(msg.To)) {
-					// user created
-				} else if (isChatGroupId(msg.To)) {
-					// group created
-				}
-				break;
-			}
-			case "server-create-and-login-as-chat-user" :
-			case "server-login-as-chat-user" : {
-				if(!isErrorEvent(msg)) {
-					chatter.User = GetUserFromString(msg.Message)
-					chatter.CurrentState = STATES.LOGGED_IN
-					executeOnlyAFunctionIfNotNull(chatter.onLogin)
-				}
-				break;
-			}
-			case "server-logout-from-chat-user" : {
-				if(!isErrorEvent(msg)) {
-					chatter.User = null
-					chatter.CurrentState = STATES.CONNECTED
-					executeOnlyAFunctionIfNotNull(chatter.onLogout)
-				}
-				break;
-			}
-			case "server-get-all-users" : {
-				if(!isErrorEvent(msg)) {
-					chatter.AllUsersById = {}
-					var results = msg.Messages.map(function(userStr){
-						return GetUserFromString(userStr)
-					}).filter(function(user){
-						return user != null && user.Id != Chatter.User.Id
-					})
-					executeOnlyAFunctionIfNotNull(chatter.onChangeUsersList, results)
-				}
-				break;
-			}
-			case "server-event-update" : {
-				if(!isErrorEvent(msg)) {
-					var user = GetUserFromString(msg.Message)
-					if(user != null && user.Id != Chatter.User.Id) {
-						executeOnlyAFunctionIfNotNull(chatter.onUserNotification, user)
-					}
-				}
-				break;
-			}
-			case "server-search-chatter-box" : {
-				if(!isErrorEvent(msg)) {
-					var results = msg.Messages.map(function(userStr){
-						return GetUserFromString(userStr)
-					}).filter(function(user){
-						return user != null
-					})
-					executeOnlyAFunctionIfNotNull(chatter.onSearchResultsReady, results)
-				}
-				break;
-			}
-		}
-	} else {
-		chatter.onChatMessage(msg)
-	}
-}
 
 function GetUserFromString(userStr) {
 	var userData = userStr.split(',')
